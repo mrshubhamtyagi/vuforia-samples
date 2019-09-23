9fb0a8a642e7c26dcb00684488c8bd7bb5670975
Shubham Tyagi <styagi.2301@gmail.com>
2019-09-24 00:03:58 +0530

    Added Database

---
+++ Assets/_MyStuff/Scripts/MyTrackerHandler.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class MyTrackerHandler : MonoBehaviour, ITrackableEventHandler
+{
+
+    public TrackableBehaviour trackableBehaviour;
+
+    private void Awake()
+    {
+        trackableBehaviour = GetComponent<TrackableBehaviour>();
+    }
+
+    void Start()
+    {
+        if (trackableBehaviour == null)
+        {
+            Debug.LogError("TrackableBehaviour is NULL");
+        }
+        else
+        {
+            trackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            TrackerManager.Instance.GetTracker<ObjectTracker>().Start();
+        }
+    }
+
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            print("Found");
+            TrackerManager.Instance.GetTracker<ObjectTracker>().Stop();
+        }
+        else
+        {
+            print("Lost");
+        }
+    }
+}
